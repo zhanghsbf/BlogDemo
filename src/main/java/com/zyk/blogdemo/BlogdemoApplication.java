487903b90dc36fa58b7d78014d03e3be9977b57a
zhangyunkun <1019437875@qq.com>
2019-05-28 20:34:59 +0800

    provide a hello demo

---
+++ src/main/java/com/zyk/blogdemo/BlogdemoApplication.java
@@ -0,0 +1,13 @@
+package com.zyk.blogdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogdemoApplication.class, args);
+    }
+
+}
